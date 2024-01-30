c27cc91147d9d7966c16e4019f30fd0a50ef644f
xFTPanda <90429130+xFTPanda@users.noreply.github.com>
2024-01-30 03:11:03 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""],
+  ownerID: ["747313012171341916", ""],
   botInvite: "",
   supportServer: "",
   mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
